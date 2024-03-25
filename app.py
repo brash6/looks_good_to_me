ac192bb5bf5051d0bfa32cb0542a6e79d327e63d
Hadrien Mariaccia <hadrienmariaccia@gmail.com>
2024-03-25 16:25:21 +0100

    Add current app and backend files

---
+++ app.py
@@ -0,0 +1,41 @@
+import streamlit as st
+from openai import OpenAI
+
+from config import OPENAI_API_KEY
+from package_review import get_file_content, get_package_review
+
+st.write('# Looks good to me')
+st.write('## 1- Enter your OpenAI API key')
+api_key = st.text_input('OpenAI API key', type='password',
+                        label_visibility="collapsed")
+
+client = OpenAI(api_key=api_key)
+
+t1, t2 = st.tabs(['Review package structure', 'Review a specific file'])
+
+with t1:
+    # Title and input field for GitHub repository URL
+    st.title("Python Package Structure Review")
+    repo_url_1 = st.text_input("Enter GitHub repository URL", key="1")
+    branch_to_review = st.text_input(
+        "Enter the name of the branch you want to review", key="2")
+
+    # Button to trigger review
+    if st.button("Review Package"):
+        # Call backend function to retrieve and display review
+        review_struc = get_package_review(repo_url_1, branch_to_review, client)
+        st.write(review_struc)
+
+with t2:
+
+    # Title and input field for GitHub repository URL
+    st.title("Python File Review")
+    repo_url_2 = st.text_input("Enter GitHub repository URL", key="3")
+    file_path = st.text_input(
+        "Enter the path of the python file you want to review", key="4")
+
+    # Button to trigger review
+    if st.button("Review File"):
+        # Call backend function to retrieve and display review
+        review_file = get_file_content(repo_url_2, file_path, client)
+        st.write(review_file)
---
+++ package_review.py
@@ -0,0 +1,93 @@
+import requests
+import json
+import numpy as np
+import base64
+from openai import OpenAI
+
+
+def get_tree_from_github_repo_api(tree, depth=0):
+    repo_tree = ""
+    for item in tree:
+        len_prev = depth
+        if item['type'] == 'tree':
+            repo_tree = repo_tree + '│    ' * depth + \
+                '├── ' + item['path'].split("/")[-1] + "\n"
+            depth = len_prev = depth + 1
+        else:
+            cur_path_length = len(item['path'].split("/"))
+            if cur_path_length < len_prev:
+                depth = depth - np.abs(cur_path_length - (len_prev + 1))
+            repo_tree = repo_tree + '│    ' * depth + \
+                '├── ' + item['path'].split("/")[-1] + "\n"
+            len_prev = cur_path_length
+
+    print(repo_tree)
+    return repo_tree
+
+
+def get_repository_tree(repo_url, branch_to_review='main'):
+    # Parse GitHub repository URL to get owner and repository name
+    owner, repo = repo_url.split("/")[-2:]
+    # Send GET request to GitHub API to retrieve repository tree
+    url = f"https://api.github.com/repos/{owner}/{repo}/git/trees/{branch_to_review}?recursive=1"
+    response = requests.get(url)
+    tree_dict = json.loads(response.content)
+    tree = get_tree_from_github_repo_api(tree_dict['tree'])
+    return tree, repo
+
+
+def get_package_review(repo_url, branch_to_review, client):
+    # Retrieve repository tree
+    tree, repo = get_repository_tree(
+        repo_url, branch_to_review=branch_to_review)
+
+    input_prompt = f"Here is the folders and files structure of a python package called {repo} \n"
+    input_prompt += f"{tree}\n"
+    input_prompt += f"If you think this package folders and files structure can be improved, can you suggest another structure by keeping the same tree format ?"
+
+    response = client.chat.completions.create(
+        model="gpt-4",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant specialized in software engineering in python."},
+            {"role": "user", "content": f"{input_prompt}"},
+        ])
+
+    return response.choices[0].message.content
+
+
+def get_file_content(repo_url, file_path, client):
+    owner, repo = repo_url.split("/")[-2:]
+    url = f"https://api.github.com/repos/{owner}/{repo}/contents/{file_path}"
+    headers = {
+        "Accept": "application/vnd.github.v3+json"
+    }
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        content = base64.b64decode(response.json()["content"]).decode("utf-8")
+
+        input_prompt = f"Here is a python file {file_path} of a github repository called {repo} \n"
+        input_prompt += f"{content}\n"
+        input_prompt += f"Can you make suggestions to improve this python file ?"
+
+        response = client.chat.completions.create(
+            model="gpt-4",
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant specialized in software engineering in python."},
+                {"role": "user", "content": f"{input_prompt}"},
+            ])
+
+        return response.choices[0].message.content
+    else:
+        print(f"Error: {response.status_code} - {response.text}")
+        return None
+
+
+if __name__ == "__main__":
+
+    client = OpenAI(
+        api_key='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')
+
+    repo_link = "https://github.com/hi-paris/deepdespeckling"
+    suggestions = get_package_review(repo_link, client=client)
+
+    print(suggestions)
