b44ee7bd4c4fa1d53d91f539700284ad8349a5b5
Hadrien Mariaccia <hadrienmariaccia@gmail.com>
2024-03-25 16:54:55 +0100

    update app file

--- app.py
+++ app.py
@@ -1,40 +1,51 @@
+import os
 import streamlit as st
 from openai import OpenAI
 
 from package_review import get_file_content, get_package_review
 
-st.write('# Looks good to me')
+st.set_page_config(
+    page_title="Looks Good To Me",
+    page_icon="🧊")
+
+ss = st.session_state
+
+st.title
 st.write('## 1- Enter your OpenAI API key')
-api_key = st.text_input('OpenAI API key', type='password',
+api_key = st.text_input('OpenAI API key', type='password', key="api_key",
                         label_visibility="collapsed")
 
-client = OpenAI(api_key=api_key)
+# Getting secret API
+OPEN_AI_KEY = ss.get('api_key')
+client = OpenAI(api_key=OPEN_AI_KEY)
 
 t1, t2 = st.tabs(['Review package structure', 'Review a specific file'])
 
 with t1:
-    # Title and input field for GitHub repository URL
+    # Reviewing package structure
     st.title("Python Package Structure Review")
     repo_url_1 = st.text_input("Enter GitHub repository URL", key="1")
     branch_to_review = st.text_input(
         "Enter the name of the branch you want to review", key="2")
 
-    # Button to trigger review
     if st.button("Review Package"):
-        # Call backend function to retrieve and display review
-        review_struc = get_package_review(repo_url_1, branch_to_review, client)
-        st.write(review_struc)
+        try:
+            review_struc = get_package_review(
+                repo_url_1, branch_to_review, client)
+            st.write(review_struc)
+        except Exception as error:
+            st.write(error)
 
 with t2:
-
-    # Title and input field for GitHub repository URL
+    # Reviewing a specific file
     st.title("Python File Review")
     repo_url_2 = st.text_input("Enter GitHub repository URL", key="3")
     file_path = st.text_input(
         "Enter the path of the python file you want to review", key="4")
 
-    # Button to trigger review
     if st.button("Review File"):
-        # Call backend function to retrieve and display review
-        review_file = get_file_content(repo_url_2, file_path, client)
-        st.write(review_file)
+        try:
+            review_file = get_file_content(repo_url_2, file_path, client)
+            st.write(review_file)
+        except Exception as error:
+            st.write(error)
